f0eb1cd2ec09d933d4d889eb56ec48bd35d23171
ryugarret <67803281+ryugarret@users.noreply.github.com>
2021-12-16 23:54:58 +0000

    Reestructuración de los paquetes para mejor orden y uniformidad. corregidas las url en las entregas de los ejercicios.

---
+++ src/FranciscoGarretaCalderon/Tema3EjerciciosJava/ConcatenarTextos.java
@@ -0,0 +1,15 @@
+package FranciscoGarretaCalderon.Tema3EjerciciosJava;
+
+public class ConcatenarTextos {
+
+    public static void main(String[] args) {
+
+        String[] textos = {"Este programa", "al ejecutarse","debería mostrar", "todos estos", "textos concatenados."};
+        String textoCompleto = "";
+
+        for(String texto : textos) textoCompleto = textoCompleto + " " + texto;
+
+        System.out.println(textoCompleto);
+
+    }
+}
