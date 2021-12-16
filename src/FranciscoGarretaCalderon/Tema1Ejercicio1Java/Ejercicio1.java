a63d3f88b2cb66109bda690148b1945306b85ed9
ryugarret <67803281+ryugarret@users.noreply.github.com>
2021-12-16 21:29:36 +0000

    Ejercicio 1 Completado.

---
+++ src/FranciscoGarretaCalderon/Tema1Ejercicio1Java/Ejercicio1.java
@@ -0,0 +1,36 @@
+package FranciscoGarretaCalderon.Tema1Ejercicio1Java;
+
+public class Ejercicio1 {
+
+    public static void main(String[] arg){
+
+        // enteros
+        byte entero1 = 1;
+        System.out.println(entero1);
+        short entero2 = 42;
+        System.out.println(entero2);
+        int entero3 = 107;
+        System.out.println(entero3);
+        long entero4 = 1024;
+        System.out.println(entero4);
+
+        // flotantes
+        float flotante1 = 7.2f;
+        System.out.println(flotante1);
+        double flotante2 = 6.32d;
+        System.out.println(flotante2);
+
+        // boolean
+        boolean verdadero = true;
+        System.out.println(verdadero);
+        boolean falso = false;
+        System.out.println(falso);
+
+        // texto
+        String cadenaDeTexto = "¡Java es lo mejor!";
+        System.out.println(cadenaDeTexto);
+        char caracter = 'i';
+        System.out.println(caracter);
+
+    }
+}
