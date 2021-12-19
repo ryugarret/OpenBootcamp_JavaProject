6eb15372595f8b1fb28f74dce84aec0084112245
ryugarret <67803281+ryugarret@users.noreply.github.com>
2021-12-19 12:10:55 +0000

    Revisión del ejercicio de sumar el IVA a un precio y cambio de double por BigDecimal por el uso de divisas.

--- src/FranciscoGarretaCalderon/Tema2EjerciciosJava/CalcularIVA.java
+++ src/FranciscoGarretaCalderon/Tema2EjerciciosJava/CalcularIVA.java
@@ -1,20 +1,26 @@
 package FranciscoGarretaCalderon.Tema2EjerciciosJava;
+import java.math.BigDecimal;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CalcularIVA {
 
     public static void main(String[] args) {
 
-        double precio = ThreadLocalRandom.current().nextDouble(0.05, 1500);
+        double precioAleatorio = ThreadLocalRandom.current().nextDouble(0.05, 1500);
+        BigDecimal precio = new BigDecimal(precioAleatorio);
+
         System.out.format("El precio sin IVA es %.2f €\n", precio);
         total(precio);
 
     }
 
-    static void total(double precio){
+    static void total(BigDecimal precio){
 
-        double iva = precio * 0.21;
-        double total = iva + precio;
+        double ivadouble = 0.21;
+        BigDecimal porcientoDeIva = new BigDecimal(ivadouble);
+
+        BigDecimal iva = precio.multiply(porcientoDeIva);
+        BigDecimal total = iva.add(precio);
         System.out.format("El precio total es %.2f €", total);
 
     }
